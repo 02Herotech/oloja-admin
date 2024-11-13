33557e3e54d35862c444d904e517bd7c8104b19c
Ntami Egbe <77502425+ntamiegbe@users.noreply.github.com>
2024-11-13 14:20:36 +0100

    chore: added suspense wrapper

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,12 +1,17 @@
 import Login from "@/components/auth/Login";
 import { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
     title: "Oloja Admin | Sign In",
 };
 
 const SignInPage = () => {
-    return <Login />;
+    return (
+        <Suspense>
+            <Login />
+        </Suspense>
+    );
 };
 
 export default SignInPage;
