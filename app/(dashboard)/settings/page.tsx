f625ffe2bce5e2b6a1a9cc4cea2dc34987ef512a
Ntami Egbe <77502425+ntamiegbe@users.noreply.github.com>
2024-11-25 14:58:11 +0100

    fix:

--- app/(dashboard)/settings/page.tsx
+++ app/(dashboard)/settings/page.tsx
@@ -1,8 +1,18 @@
+"use client"
+
+import Button from '@/components/global/Button'
+import { signOut } from 'next-auth/react'
 import React from 'react'
 
 const Settings = () => {
+
   return (
-    <div>Settings</div>
+    <div>
+      <h2>Settings</h2>
+      <Button onClick={() => signOut()}>
+        Logout
+      </Button>
+    </div>
   )
 }
 
